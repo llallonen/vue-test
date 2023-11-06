8f23882f104381ff1596aafaa48e1b9ec535011e
llallonen <llallonen@gmail.com>
2023-11-06 14:35:57 +0300

    feat: update contact component

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,33 +1,45 @@
 <script setup lang="ts">
 import type { PropType } from "vue";
-
-interface ContactData {
-  name: String;
-  type: Number;
-  phone: String;
-  source: Number;
-  createdAt: "2023-11-02";
-  updatedAt: "2023-11-02";
-}
-
-interface Contact {
-  id: Number;
-  data: ContactData;
-  createdAt: String;
-  updatedAt: String;
-}
+import type { Contact } from "~/interfaces";
 
 const props = defineProps({
   contact: Object as PropType<Contact>,
+  kind: {
+    type: String,
+  },
 });
+
+const typeMap = [
+  { type: "call", name: "Звонок" },
+  { type: "callRequest", name: "Заявка на обратный звонок" },
+  { type: "calcRequest", name: "Заявка на расчет" },
+];
+
+const sourceMap = [
+  { type: "direct", name: "Яндекс.Директ" },
+  { type: "seo", name: "SEO" },
+  { type: "avito", name: "Авито" },
+];
 </script>
 
 <template>
-
   <div
-    class="flex flex-col rounded-md border-secondary border-2 p-3 bg-neutral shadow-sm hover:shadow-md hover:shadow-secondary hover:cursor-pointer h-[100px]"
+    v-if="contact"
+    class="flex flex-row justify-between rounded-md border-secondary border-2 p-3 bg-neutral shadow-sm hover:shadow-md hover:shadow-secondary hover:cursor-pointer h-[100px]"
   >
-    <h2 class="text-accent mt-auto text-lg">{{ contact?.data.name }}</h2>
-    <h3 class="text-sm text-primary">{{ contact?.data.phone }}</h3>
+    <div class="w-6/12">
+      <h2 class="text-accent mt-auto text-xl">{{ contact?.data.name }}</h2>
+      <h3 class="text-md text-primary">{{ contact?.data.phone }}</h3>
+    </div>
+    <div class="w-5/12 text-lg">
+      <div :class="[`text-${typeMap[contact.data.type].type}`]">
+        {{ typeMap[contact.data.type].name }}
+      </div>
+      <div :class="[`text-${sourceMap[contact.data.source].type}`]">
+        {{ sourceMap[contact.data.source].name }}
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped></style>
